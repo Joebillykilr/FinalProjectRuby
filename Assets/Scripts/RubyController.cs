80d941c1b355b7e468d0083a9466615741bd9d9b
Alex Shopovick <ashopovickthegreat@gmail.com>
2021-04-23 15:06:06 -0400

    First commit

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RubyController : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+    public GameObject projectilePrefab;
+
+    public int score;
+    public Text scoreText;
+
+    public bool gameOver = false;
+    public static bool staticVar = false;
+
+    public GameObject player;
+
+    public Text winText;
+
+    public AudioClip throwSound;
+    public AudioClip hitSound;
+
+    public ParticleSystem damageEffect;
+    public ParticleSystem healEffect;
+
+    public int health { get { return currentHealth; }}
+    int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal; 
+    float vertical;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+    AudioSource audioSource;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        currentHealth = maxHealth;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        score = 0;
+        SetScoreText ();
+        winText.text = "";
+        audioSource= GetComponent<AudioSource>();
+        player = GameObject.FindWithTag("RubyController");
+    }
+
+    // Update is called once per frame
+  void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+        
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+        
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+
+        if(health == 0)
+        {
+            winText.text = "You lost! Press R to restart";
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+
+            if (gameOver == true)
+            {
+
+              SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // this loads the currently active scene
+
+            }
+
+        }
+    
+    
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+        RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+           if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+            damageEffect.Play();
+            PlaySound(hitSound);
+        }
+
+        if (amount > 0)
+        {
+            healEffect.Play();
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
+    }
+
+    public void ChangeScore(int scoreAmount)
+    {
+        score = score + 1;
+        SetScoreText ();
+    }
+
+    void SetScoreText ()
+    {
+        scoreText.text = "Robots Fixed: " + score.ToString ();
+        if (score == 4) 
+        {
+            winText.text = "You Win! Game created by Alexander Shopovick";
+        }
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+
+        PlaySound(throwSound);
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+
+}
